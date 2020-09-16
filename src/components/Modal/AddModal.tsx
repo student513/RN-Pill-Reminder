ec29941807ee84d8b5bf6ccab89def91145874dc
johyeongjun <compa513@naver.com>
2020-09-16 17:22:06 +0900

    clear code

--- src/components/Modal/AddModal.tsx
+++ src/components/Modal/AddModal.tsx
@@ -147,13 +147,6 @@ class AddModal extends Component<AddModalProps, {}> {
               description="Critical alerts allows the app to ring the notification sound even when your phone is in silent or do not disturb mode."
             />
           )}
-          {/* <TouchableOpacity
-            onPress={() => {
-              console.log(setCycleStore.Bedtime);
-              console.log(setCycleStore.Critical);
-            }}>
-            <MyText style={{marginBottom: 30}}>ddddddd</MyText>
-          </TouchableOpacity> */}
         </ScrollView>
       </Modal>
     );
