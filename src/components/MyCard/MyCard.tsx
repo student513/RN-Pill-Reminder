a4f595709ec7496f121ea035314140ed830de0d3
johyeongjun <compa513@naver.com>
2020-09-26 17:37:29 +0900

    add flat MyCard (no function, no conditional style)

--- src/components/MyCard/MyCard.tsx
+++ src/components/MyCard/MyCard.tsx
@@ -6,15 +6,31 @@ import {MyText} from 'components/MyText';
 interface IProps {
   name: string;
   dosage: string;
+  timing: string;
 }
 
 export class MyCard extends PureComponent<IProps, {}> {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.before}>
-          <MyText>{this.props.name}</MyText>
-          <MyText>{this.props.dosage}</MyText>
+        <TouchableOpacity style={styles.card}>
+          <TouchableOpacity>
+            <View style={styles.circle} />
+          </TouchableOpacity>
+          <View style={styles.name}>
+            <MyText style={{fontSize: 17, fontFamily: 'ProximaNova-Bold'}}>
+              {this.props.name}
+            </MyText>
+          </View>
+          <View style={styles.dosage}>
+            <MyText
+              style={{fontSize: 12, paddingVertical: 1, paddingHorizontal: 5}}>
+              {this.props.dosage}
+            </MyText>
+          </View>
+          <View style={styles.timing}>
+            <MyText style={{fontSize: 12}}>1 hours ago</MyText>
+          </View>
         </TouchableOpacity>
       </View>
     );
@@ -24,10 +40,44 @@ export class MyCard extends PureComponent<IProps, {}> {
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
+    marginHorizontal: 15,
+    marginBottom: 10,
   },
-  before: {
+  circle: {
+    width: 24,
+    height: 24,
+    borderRadius: 12, //원: borderRadius는 width, height의 절반이어야함
+    borderWidth: 2,
+    borderColor: '#13A45B',
+    marginTop: 18,
+    marginBottom: 35,
+    marginLeft: 20,
+    marginRight: 25,
+  },
+  name: {
+    marginTop: 15,
+  },
+  dosage: {
+    backgroundColor: '#fff',
+    marginBottom: 40,
+    marginTop: 15,
+    borderRadius: 7,
+    position: 'absolute',
+    right: 15,
+  },
+  card: {
     backgroundColor: '#F0F9F0',
+    borderRadius: 15,
+    flexDirection: 'row',
   },
+  timing: {
+    position: 'absolute',
+    marginTop: 45,
+    left: 75,
+  },
+  before: {},
   now: {},
-  after: {},
+  after: {
+    backgroundColor: '#931AB7',
+  },
 });
