1cf06b5aa145c81cb7eb68bc811daf4e305d131a
jojo <compa513@naver.com>
2020-10-11 22:08:07 +0900

    add reminder view with image
    need to adjust guide comment height

--- src/screens/Reminder.tsx
+++ src/screens/Reminder.tsx
@@ -1,35 +1,76 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import {SetTypeModal} from 'components/Modal';
 import {MyText} from 'components/MyText';
 import {observer} from 'mobx-react';
 import {controlModalStore} from 'store/ControlModal';
 import {MyCard} from 'components/MyCard';
 import {pillListStore} from 'store';
+import moment from 'moment';
 
 interface IProps {
   navigation: any;
 }
 
 @observer
-class Reminder extends Component<IProps, {}> {
+class Reminder extends Component<IProps, {today: string}> {
   constructor(props: any) {
     super(props);
+    this.state = {
+      today: '',
+    };
   }
+  componentDidMount = () => {
+    const date = new Date();
+    this.setState({today: moment.parseZone(date).format('dddd, MMMM D')});
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity
-          onPress={() => {
-            controlModalStore.toggleSetTypeModalVisible();
-          }}>
-          <MyText style={{fontSize: 30}}>Add</MyText>
-        </TouchableOpacity>
-
-        {pillListStore.CardList.map((pill) => (
-          <MyCard name={pill.Name} dosage={pill.Dosage} />
-        ))}
-
+      <View
+        style={
+          pillListStore.CardList.length > 0
+            ? styles.container
+            : styles.containerNone
+        }>
+        <View>
+          <TouchableOpacity
+            style={{alignItems: 'flex-end'}}
+            onPress={() => {
+              controlModalStore.toggleSetTypeModalVisible();
+            }}>
+            <MyText style={{fontSize: 30, marginTop: 50}}>Add</MyText>
+          </TouchableOpacity>
+          <MyText style={styles.tabTitle}>Reminder</MyText>
+          <MyText
+            style={
+              pillListStore.CardList.length > 0
+                ? styles.today
+                : styles.todayNone
+            }>
+            {this.state.today}
+          </MyText>
+        </View>
+        <View>
+          {pillListStore.CardList.length > 0 ? (
+            pillListStore.CardList.map((pill) => (
+              <MyCard name={pill.Name} dosage={pill.Dosage} />
+            ))
+          ) : (
+            <>
+              <View style={{alignItems: 'center'}}>
+                <MyText style={styles.guideComment}>
+                  Touch the + button and start.
+                </MyText>
+              </View>
+              <View style={{alignItems: 'center'}}>
+                <Image
+                  source={{uri: 'BackgroundImg'}}
+                  style={styles.backgroundImage}
+                />
+              </View>
+            </>
+          )}
+        </View>
         <SetTypeModal
           isVisible={controlModalStore.setTypeModalVisible}
           onSwipeComplete={() => controlModalStore.toggleSetTypeModalVisible()}
@@ -44,12 +85,46 @@ class Reminder extends Component<IProps, {}> {
   }
 }
 const styles = StyleSheet.create({
-  container: {
+  containerNone: {
+    flex: 1,
     backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
   },
   modal: {
     justifyContent: 'flex-end',
     margin: 0,
+  },
+  tabTitle: {
+    fontSize: 36,
+    fontFamily: 'ProximaNova-Bold',
+    color: '#393939',
+    marginLeft: 10,
+  },
+  todayNone: {
+    fontSize: 21,
+    color: '#5B5B5B',
+    marginLeft: 10,
+  },
+  today: {
+    fontSize: 21,
+    color: '#5B5B5B',
+    marginLeft: 10,
+    marginBottom: 30,
+  },
+  backgroundImage: {
+    width: 330,
+    height: 277,
+    bottom: -35,
+  },
+  guideComment: {
+    fontSize: 21,
+    color: '#393939',
   },
 });
 
